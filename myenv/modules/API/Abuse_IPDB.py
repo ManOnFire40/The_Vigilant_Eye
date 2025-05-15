9035d9ed23ef3a012f9067357444617bf4370cd0
Ehab <manOnFire4020@gmail.com>
2025-05-15 18:55:17 +0300

    Back_End_Done

---
+++ myenv/modules/API/Abuse_IPDB.py
@@ -0,0 +1,93 @@
+import requests
+import json
+
+IPDB_API = None
+
+
+def save_Virus_total_API_Key(API):
+    try:
+        with open('../API_DB/IPDB_API_Key.txt', 'w') as fp:
+            fp.write(API)
+    except FileNotFoundError as e:
+        print(f"Please check the DB path")
+        print(f"Error: {e}")
+        return (f"Error: {e}")      
+
+def load_IPDB_API_Key():
+    try:
+        with open('../API_DB/IPDB_API_Key.txt', 'r') as fp:
+            line = fp.readline()
+            while line != '':
+                IPDB_API=str(line)
+            return IPDB_API    
+    except FileNotFoundError as e:
+        print(f"Please check the DB path")
+        print(f"Error: {e}")
+        return (f"Error: {e}")       
+    
+        
+    
+    
+
+### AbuseIPDB API Integration
+def get_ip_info_ipdb(ip_address):
+    try:
+        # Defining the api-endpoint
+        url = 'https://api.abuseipdb.com/api/v2/check'
+        querystring = {
+            'ipAddress': ip_address,
+            'maxAgeInDays': '90'
+        }
+        headers = {
+            'Accept': 'application/json',
+            'Key': IPDB_API
+        }
+        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+        # Formatted output
+        decodedResponse = json.loads(response.text)
+        print (json.dumps(decodedResponse, sort_keys=True, indent=4))
+        return decodedResponse
+    except Exception as e:
+        error_message = str(f"Exception Error: {e}")
+        print(error_message)
+        return error_message
+
+
+
+def get_ips_info_ipdb(ip_addresses):
+    try:
+        # takes a list of ip addresses and returns a list of reports
+        list_of_ip_addresses_reports = []
+        for ip_address in ip_addresses:
+            list_of_ip_addresses_reports.append(get_ip_info_ipdb(ip_address, IPDB_API))
+        return list_of_ip_addresses_reports;
+    except Exception as e:
+        error_message = str(f"Exception Error: {e}")
+        print(error_message)
+        return error_message
+
+
+
+
+def get_subnet_info_ipdb(subnet):
+    try:
+        # Defining the api-endpoint
+        url = 'https://api.abuseipdb.com/api/v2/check-block'
+        querystring = {
+            'network':subnet,
+            'maxAgeInDays':'15',
+        }
+        headers = {
+            'Accept': 'application/json',
+            'Key': IPDB_API
+        }
+        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+        # Formatted output
+        decodedResponse = json.loads(response.text)
+        print (json.dumps(decodedResponse, sort_keys=True, indent=4))
+        return decodedResponse
+    except Exception as e:
+        error_message = str(f"Exception Error: {e}")
+        print(error_message)
+        return error_message
+
---
+++ myenv/modules/API/IP_info_API.py
@@ -0,0 +1,53 @@
+import requests
+
+IPINFO_API_TOKEN = None
+
+
+def save_Virus_total_API_Key(API):
+    try:
+        with open('../API_DB/IPINFO_API_TOKEN.txt', 'w') as fp:
+            fp.write(API)
+    except FileNotFoundError as e:
+        print(f"Please check the DB path")
+        print(f"Error: {e}")
+        return (f"Error: {e}")              
+
+
+
+
+def load_IPINFO_API_Key():
+    try:
+        with open('../API_DB/IPINFO_API_TOKEN.txt', 'r') as fp:
+            line = fp.readline()
+            while line != '':
+                IPINFO_API_TOKEN=str(line)
+            return IPINFO_API_TOKEN    
+    except FileNotFoundError as e:
+        print(f"Please check the DB path")
+        print(f"Error: {e}")
+        return (f"Error: {e}")        
+
+
+
+
+
+def IP_privacy_detection(ip_address):
+
+    # Construct the API URL
+    url = f'https://ipinfo.io/{ip_address}/privacy?token={IPINFO_API_TOKEN}'
+    # Make the GET request to the API
+    response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        print(f"IP Address: {ip_address}")
+        print(f"VPN: {data.get('vpn')}")
+        print(f"Tor: {data.get('tor')}")
+        print(f"Hosting Provider: {data.get('hosting')}")
+        print(f"Proxy: {data.get('proxy')}")
+        print(f"Relay: {data.get('relay')}")
+        print(f"Service: {data.get('service')}")
+        return data
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+        return "Server error"
